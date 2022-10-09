ec8bbc435c2f549a21aa9c217bdb1a35b7da59f0
Paul Fruitful <fruitful2007@outlook.com>
2022-10-09 19:36:31 +0100

    Chat Controller and Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\chatControl;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/index',[chatControl::class,'index'])->middleware('auth');
