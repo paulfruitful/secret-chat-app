e297165a8610f74acf23846b76e3380c121f8325
Paul Fruitful <fruitful2007@outlook.com>
2022-10-09 19:43:25 +0100

    Single chat

--- app/Http/Controllers/chatControl.php
+++ app/Http/Controllers/chatControl.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chat;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class chatControl extends Controller
@@ -10,4 +12,8 @@ class chatControl extends Controller
     public function index(){
         return view('chats');
     }
+    public function show(Chat $chat){
+        
+    }
+
 }
