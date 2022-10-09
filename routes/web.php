af1f739b55fc52897e750ae3e3caf97e6b0f537e
Paul Fruitful <fruitful2007@outlook.com>
2022-10-09 19:49:21 +0100

    route error fixed

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/index',[chatControl::class,'index'])->middleware('auth');
+Route::get('/chat',[chatControl::class,'index']);
