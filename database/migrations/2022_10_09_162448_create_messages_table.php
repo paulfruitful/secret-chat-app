6b75932470d720afb6a6921e58fa8c4ed4901326
Paul Fruitful <fruitful2007@outlook.com>
2022-10-09 17:26:37 +0100

    Message migration

--- database/migrations/2022_10_09_162448_create_messages_table.php
+++ database/migrations/2022_10_09_162448_create_messages_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->longText('message');
+            $table->foreignId('user_id');
+
             $table->timestamps();
         });
     }
