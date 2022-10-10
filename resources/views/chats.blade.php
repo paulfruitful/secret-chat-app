6b75afb1944acee94f4669c8e55e6a19bedb6638
Paul Fruitful <fruitful2007@outlook.com>
2022-10-10 12:45:39 +0100

    New stuffs and migrations

--- resources/views/chats.blade.php
+++ resources/views/chats.blade.php
@@ -2,7 +2,32 @@
 
 @section('content')
 
+<ul class="list-group">
+    @forelse ($chats as $chat)
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand">Navbar</a>
+        </div>
+      </nav>
+    @empty
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
+    @endforelse
+  </ul>
 
+<div class="fixed-bottom">
+<div class="mb-3">
+    <form action="/send" method="post">
+        @csrf
+    <label for="exampleFormControlTextarea1" class="form-label"></label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="3" placeholder="Send A New Message {{auth()->user()->name}}"></textarea>
+    <button type="submit" class="btn btn-primary">Send <i></i></button>      
+</form>
+  </div>
+</div>
 
 
 @endsection
