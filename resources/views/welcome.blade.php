0a40e9e927c8c5e90cc5528a6282eeac063a1f43
Paul Fruitful <fruitful2007@outlook.com>
2022-10-17 02:25:37 +0100

    View Correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,20 @@
                 </div>
             @endif
 
-           
+            <div class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="col-10 col-sm-8 col-lg-6">
+                  <img src="https://i.ibb.co/0tSMLFz/DSC-2843.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+                </div>
+                <div class="col-lg-6">
+                  <h1 class="display-5 fw-bold lh-1 mb-3">Secret Chat App</h1>
+                  <p class="lead">This app was created by Paul Fruitful for Apostle M.U Ronald and Prisa Tochi to communicate with each other without the prying eyes of the big tech organisations such as Facebook. The only person who has access to the messages  stored on this app is Paul Fruitful only if a situation arises when he ought to fix an emergent error.</p>
+                  <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                  </div>
+                </div>
+              </div>
+            </div>
     </body>
 </html>
